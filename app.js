615a7eccfaf6b88c5e1d11ccebd721daecf07e2e
aditya singh <volttop7@gmail.com>
2025-01-02 13:42:44 +0530

    Updated app.js to use dynamic port

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 
 // Middleware
 app.use(express.json());
